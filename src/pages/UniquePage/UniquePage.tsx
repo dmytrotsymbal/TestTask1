eb4ad45de95c382cae2a6fc8fa024119ac2f7102
Dima <dima.tsymbal@outlook.com>
2023-06-13 15:24:15 +0300

    finish cooking part

--- src/pages/UniquePage/UniquePage.tsx
+++ src/pages/UniquePage/UniquePage.tsx
@@ -1,7 +1,9 @@
-import { Container, Grid } from '@mui/material'
+import { Button, Container, Grid } from '@mui/material'
 import { Products, getProductsObject } from 'Utils/ProductsArrey'
 import { useParams } from 'react-router-dom'
 import { useAppSelector } from 'redux/hooks'
+import './UniquePage.scss'
+import { useState } from 'react'
 
 type Props = {}
 
@@ -9,63 +11,107 @@ type ProductsObject = {
     [id: number]: Products
 }
 
+type CheckboxState = {
+    checkbox1: boolean
+    checkbox2: boolean
+    checkbox3: boolean
+    checkbox4: boolean
+    checkbox5: boolean
+}
+
 const UniquePage = (props: Props) => {
-    const { id } = useParams()
+    const { id } = useParams<{ id: string }>()
+
+    const [checkboxes, setCheckboxes] = useState<CheckboxState>({
+        checkbox1: false,
+        checkbox2: false,
+        checkbox3: false,
+        checkbox4: false,
+        checkbox5: false,
+    })
+
+    const handleButtonClick = () => {
+        setCheckboxes({
+            checkbox1: false,
+            checkbox2: false,
+            checkbox3: false,
+            checkbox4: false,
+            checkbox5: false,
+        })
+    }
+
+    const handleCheckboxChange = (checkboxName: keyof CheckboxState) => {
+        setCheckboxes((prevCheckboxes) => ({
+            ...prevCheckboxes,
+            [checkboxName]: !prevCheckboxes[checkboxName],
+        }))
+    }
 
     const productsArrey = useAppSelector((state) => state.products) // приймаэмо масив з сховища
     const productsObject: ProductsObject = getProductsObject(productsArrey)
+
+    const product = productsObject[parseInt(id!)]
 
     return (
         <div className="UniquePage">
             <Container sx={{ padding: '20px 0' }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6} md={6}>
-                        <h3 className="UniquePage__title">
-                            {productsObject[parseInt(id!)].title}
-                        </h3>
+                        <h3 className="UniquePage__title">{product.title}</h3>
                         <p className="UniquePage__description">
-                            {productsObject[parseInt(id!)].description}
+                            {product.description}
                         </p>
                         <h6 className="UniquePage__category">
-                            Категорія: {productsObject[parseInt(id!)].category}
+                            Категорія: {product.category}
                         </h6>
 
                         <div className="cookingBlock">
+                            <h3 className="cookingBlock__title">
+                                Процес готування:
+                            </h3>
                             <ol>
                                 <li>
                                     <div className="cookingBlock__steps">
-                                        {
-                                            productsObject[parseInt(id!)]
-                                                .ingridients.name1
-                                        }
-                                        <p>
-                                            {
-                                                productsObject[parseInt(id!)]
-                                                    .extraDesc1
+                                        <input
+                                            type="checkbox"
+                                            checked={checkboxes.checkbox1}
+                                            onChange={() =>
+                                                handleCheckboxChange(
+                                                    'checkbox1'
+                                                )
                                             }
-                                        </p>
+                                        />
+                                        {product.ingridients.name1}
+                                        <p>{product.extraDesc1}</p>
                                     </div>
                                 </li>
                                 <li>
                                     <div className="cookingBlock__steps">
-                                        {
-                                            productsObject[parseInt(id!)]
-                                                .ingridients.name2
-                                        }
-                                        <p>
-                                            {
-                                                productsObject[parseInt(id!)]
-                                                    .extraDesc2
+                                        <input
+                                            type="checkbox"
+                                            checked={checkboxes.checkbox2}
+                                            onChange={() =>
+                                                handleCheckboxChange(
+                                                    'checkbox2'
+                                                )
                                             }
-                                        </p>
+                                        />
+                                        {product.ingridients.name2}
+                                        <p>{product.extraDesc2}</p>
                                     </div>
                                 </li>
                                 <li>
                                     <div className="cookingBlock__steps">
-                                        {
-                                            productsObject[parseInt(id!)]
-                                                .ingridients.name3
-                                        }
+                                        <input
+                                            type="checkbox"
+                                            checked={checkboxes.checkbox3}
+                                            onChange={() =>
+                                                handleCheckboxChange(
+                                                    'checkbox3'
+                                                )
+                                            }
+                                        />
+                                        {product.ingridients.name3}
                                         <p>
                                             Lorem ipsum dolor sit, amet
                                             consectetur adipisicing elit.
@@ -79,30 +125,32 @@ const UniquePage = (props: Props) => {
                                 </li>
                                 <li>
                                     <div className="cookingBlock__steps">
-                                        {
-                                            productsObject[parseInt(id!)]
-                                                .ingridients.name4
-                                        }
-                                        <p>
-                                            {
-                                                productsObject[parseInt(id!)]
-                                                    .extraDesc3
+                                        <input
+                                            type="checkbox"
+                                            checked={checkboxes.checkbox4}
+                                            onChange={() =>
+                                                handleCheckboxChange(
+                                                    'checkbox4'
+                                                )
                                             }
-                                        </p>
+                                        />
+                                        {product.ingridients.name4}
+                                        <p>{product.extraDesc3}</p>
                                     </div>
                                 </li>
                                 <li>
                                     <div className="cookingBlock__steps">
-                                        {
-                                            productsObject[parseInt(id!)]
-                                                .ingridients.name5
-                                        }
-                                        <p>
-                                            {
-                                                productsObject[parseInt(id!)]
-                                                    .extraDesc1
+                                        <input
+                                            type="checkbox"
+                                            checked={checkboxes.checkbox5}
+                                            onChange={() =>
+                                                handleCheckboxChange(
+                                                    'checkbox5'
+                                                )
                                             }
-                                        </p>
+                                        />
+                                        {product.ingridients.name5}
+                                        <p>{product.extraDesc1}</p>
                                     </div>
                                 </li>
                             </ol>
@@ -111,9 +159,25 @@ const UniquePage = (props: Props) => {
                     <Grid item xs={12} sm={6} md={6} key={id}>
                         <img
                             className="UniquePage__image"
-                            src={productsObject[parseInt(id!)].images}
+                            src={product.images}
                             alt="UniquePage__image"
                         />
+
+                        <div className="buttonsBlock">
+                            <Button
+                                onClick={handleButtonClick}
+                                className="buttonsBlock__button"
+                                variant="outlined"
+                            >
+                                Reset
+                            </Button>
+                            <Button
+                                className="buttonsBlock__button"
+                                variant="outlined"
+                            >
+                                Stop cooking
+                            </Button>
+                        </div>
                     </Grid>
                 </Grid>
             </Container>
